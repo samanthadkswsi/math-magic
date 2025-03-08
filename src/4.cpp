7098fb4599ac1c2d2cbb65a524179ee749c69891
samanthadkswsi <samanthadkswsi@gmx.com>
2025-03-08 06:08:40 +0000

    Add 4.cpp

---
+++ src/4.cpp
@@ -0,0 +1,7 @@
+int main() {
+  float x = 3.14;
+  int y = 2;
+  std::cout << "The value of pi is approximately: " << x << std::endl;
+  std::cout << "The sum of " << x << " and " << y << " is " << (x + y) << std::endl;
+  return 0;
+}
